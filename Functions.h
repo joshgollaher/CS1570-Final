637f7ab89859bdcdfdf7ddba976cee6b0ec8f972
Ben <bpbg54@umsystem,edu>
2023-04-17 14:55:04 -0500

    Bingus

--- Functions.h
+++ Functions.h
@@ -1,7 +1,9 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
+#include <iostream>
 #include <utility>
+#include <cstdlib>
 using namespace std;
 
 class City {
@@ -43,6 +45,7 @@ private:
     int m_robber_id;
     pair<int, int> m_robber_coordinates;
     char m_bag [17];
+    int m_bag_size = 0;
     static int m_loot_count;
     bool m_active;
 
@@ -51,6 +54,31 @@ private:
         Greedy
     };
     RobberType m_type;
+
+    void pick_up_loot(char loot) {
+        if(m_bag_size < 17){
+            m_bag[m_bag_size++] = loot;
+        }
+    }
+
+    bool is_valid_position(pair<int, int> position){
+        const int x = position.first;
+        const int y = position.second;
+
+        return x >= 0 && x <= 9 && y >= 0 && y <= 9;
+    }
+
+    void move() {
+        int delta_x, delta_y = 0;
+
+        do {
+            delta_x = rand() % 3 - 1;
+            delta_y = rand() % 3 - 1;
+        } while(delta_x == 0 && delta_y == 0 && is_valid_position(m_robber_coordinates + make_pair(delta_x, delta_y)));
+
+        m_robber_coordinates.first += delta_x;
+        m_robber_coordinates.second += delta_y;
+    }
 
 public:
 
